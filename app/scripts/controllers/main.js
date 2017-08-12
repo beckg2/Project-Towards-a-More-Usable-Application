12d751f01eb826ee2a17c8616cf8cd3dec870b37
George <beckg2@seattleu.edu>
2017-08-12 13:47:12 -0700

    data added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,12 @@
  * Controller of the wats4000ProjectTowardsAMoreUsableApplicationApp
  */
 angular.module('wats4000ProjectTowardsAMoreUsableApplicationApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, current) {
+    	$scope.current	=	current.query();
+
+      $scope.refreshCurrent	=	function(){
+        $scope.current	=	current.query({
+          location:	$scope.location
+        });
+      };
   });
---
+++ app/scripts/services/current.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name wats4000ProjectTowardsAMoreUsableApplicationApp.current
+ * @description
+ * # current
+ * Factory in the wats4000ProjectTowardsAMoreUsableApplicationApp.
+ */
+angular.module('wats4000ProjectTowardsAMoreUsableApplicationApp')
+.factory('current',	function	($resource)	{
+  //	Service	logic
+  //	...
+
+  //	Public	API	here
+  return	$resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=a8e3a9a92d14fe6ae7e920debac049d1',	{},	{
+    query:	{
+      method:'GET',
+      params:{
+        location:	'Seattle,us'
+      },
+      isArray:false
+    }
+  });
+});
